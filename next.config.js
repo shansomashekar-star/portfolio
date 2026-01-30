dec3ea4448a4215b511e40fd416d364461be3655
shan.somashekar-star <shan.somashekar@gmail.com>
2026-01-30 10:19:55 -0500

    config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['github.com', 'avatars.githubusercontent.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'github.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com', pathname: '/**' },
+    ],
   },
 }
 
